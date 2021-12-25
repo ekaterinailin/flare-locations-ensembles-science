1dcc2183ee2d8accc5ac3403117264c657b6ef10
Ekaterina Ilin <eilin@aip.de>
2021-12-25 17:51:11 +0100

    flares.py MVP complete

--- flares.py
+++ flares.py
@@ -5,8 +5,166 @@ Ekaterina Ilin
 MIT License (2022)
 """
 
+import warnings
+
 import numpy as np
+
 from altaipony.altai import aflare
+from altaipony.utils import generate_random_power_law_distribution
+
+
+def flare_contrast(t, n_spots, emin, emax, alpha, beta, n_inclinations):
+    """Creates a set of flaring light curves.
+    
+    Parameters:
+    -----------
+    t : np.array
+        array of phases in [0,1]
+    n_spots : int >=1
+        number of flaring spots, i.e. active regions
+    emin : list of floats
+        len(emin)=n_spots
+        minimum energy that the spot can produce 
+        OR detection limit, in ED space
+    emax : list of floats
+        len(emax)=n_spots
+        maximum energy that the
+        spot can produce, in ED space
+    alpha : list of floats
+        len(alpha)=n_spots
+        FFD power law exponent, in ED space
+    beta : list of floats
+        len(beta)=n_spots
+        FFD power law offset, in ED space
+    n_inclinations : int
+        i.e., number of stars
+    
+    Return:
+    --------
+    np.ndarray with dimensions (len(t), n_spots, n_inclinations),
+    i.e., a matrix of n_spots x n_inclination flare light curves
+    of length len(t), each with quiescent flux=1
+    """
+    
+    # Check if number of spots is int
+    # If not, warn user:
+    if type(n_spots) != int:
+        warnings.warn("Number of spots not an integer. "
+                      "Value will be floored to next integer.")
+    
+    
+    # expand the time series to the number of spots
+    t2d = np.repeat(np.array([t]), repeats=n_spots, axis=0)
+                    
+    # expand the time series to the number of stars
+    t3d = np.repeat(t2d.T[:,:,np.newaxis], repeats=n_inclinations, axis=2)
+    
+    # define a nested function to wrap the 3d array
+    def apply_per_spot(t3d, emin, emax, alpha, beta):
+        # create a flare light curve for each star
+        # with the same flaring region parameters
+        return np.apply_along_axis(create_flare_light_curve, 0,
+                                   t3d, emin, emax, alpha, beta)
+    
+    # if floats are passed, make list
+    listify = lambda a: [a] if (isinstance(a, float) | isinstance(a, int)) else a
+    emin, emax, alpha, beta = listify(emin), listify(emax), listify(alpha), listify(beta)
+    
+    # Check dimensions. If they don't fit, throw error:
+    if ~(np.array([len(alpha), len(emin), len(emax), len(beta)]) == int(np.floor(n_spots))).all():
+        raise ValueError(f"Check inputs: one or multiple of emin, emax, alpha, beta"
+                         f" don't match in size, which should be {int(np.floor(n_spots))}.")
+    
+    # apply function above for each spot
+    flares =  np.array([apply_per_spot(t3d[:,i,:], emin[i], emax[i],
+                                       alpha[i], beta[i]) for i in range(t3d.shape[1])])
+    
+    # transpose to make it ready to be passed to 
+    # Star.light_curve
+    return np.transpose(flares, (1, 0, 2))
+
+
+def mock_decompose_ed(ed, afactor=100., fixed_fwhm=.01):
+    """Take ED of a flare and return
+    a fake split in amplitude and FWHM.
+    Amplitude is actually nearly linear.
+    FWHM has much more spread.
+    
+    Parameters:
+    -----------
+    ed : float
+        ED of a flare
+    afactor : float, default 100
+        divide by afactor to convert ED 
+        to amplitude
+    fixed_fwhm : float, default .01
+        fix FWHM to something shorter
+        than the full phase, i.e. 1
+        
+    Return:
+    -------
+    amplitude, FWHM - float, float
+    """
+    # turn into array if just float
+    if type(ed) is float:
+        ed = np.array([ed])
+    
+    # divide ed by scale factor to get amplitude
+    # fix fwhm to something short
+    return ed / afactor, np.full_like(ed, fixed_fwhm)
+
+
+
+
+def create_flare_light_curve(time, emin, emax, alpha, beta, 
+                             decompose_ed=mock_decompose_ed, **kwargs):
+    """Generate a flare light curve using the flare
+    model from Davenport(2014) and power law disributed
+    flare energies.
+    
+    Parameters:
+    -----------
+    time : np.array
+        time series
+    emin : float
+        minimum energy that the spot can produce 
+        OR detection limit, in ED space
+    emax : float
+        maximum energy that the
+        spot can produce, in ED space
+    alpha : float
+        FFD power law exponent, in ED space
+    beta : float
+        FFD power law offset, in ED space
+    decompose_ed : func
+        function that takes ED and returns
+        amplitude and FWHM of the flare.
+        Default at the moment: mock function
+        mock_decompose_ed to be replaced later
+    kwargs : dict
+        keyword arguments to pass to decompose_ed
+        
+    Return:
+    --------
+    np.array with the same length as time, that is a light curve
+    that contains a number of flares specified by the input parameters
+    in relative flux units
+    """
+    # generate power law distributed flare energies
+    EDs = generate_random_power_law_distribution(emin, emax, alpha+1, beta)
+    
+    # generate start times of flare randomly
+    tstart = (np.random.rand(len(EDs)) * (time[-1] - time[0])) + time[0]
+    
+    # decompose ED
+    a, fwhm = decompose_ed(EDs, **kwargs)
+    
+    # generate each flare and sum up the flux. 
+    # Add 1 as baseline, because the amplitude is given in relative units
+    lc = (np.array([wrapped_aflare(time, tstart[i], fwhm[i], a[i])[1] for i, ed in enumerate(EDs)]).sum(axis=0) + 1.)
+    
+    return lc
+
 
 
 def wrapped_aflare(time, tstart, fwhm, a, a_threshold=0.001):
--- tests/test_flares.py
+++ tests/test_flares.py
@@ -1,7 +1,120 @@
 import pytest
 import numpy as np
 
-from .. import wrapped_aflare
+from ..flares import (wrapped_aflare,
+                mock_decompose_ed,
+                create_flare_light_curve,
+                flare_contrast,
+                )
+
+def test_flare_contrast1():
+    """Test #1. Integration and unit tests with either
+    multiple or a single spot. Check mostly if 
+    units are conserved. Test #2 goes for more 
+    dimension checks."""
+    
+    # MULTIPLE SPOTS
+    # give inputs as arrays
+    t = np.arange(0, 2 * np.pi, 2 * np.pi/600) 
+    n_inclinations = 100
+    n_spots = 2
+    alpha = [-1.6] * n_spots
+    beta = [20] * n_spots
+    emin = [1] * n_spots
+    emax = [100] * n_spots
+
+
+    # generate lcs
+    flares = flare_contrast(t, n_spots, emin, emax, alpha, beta, n_inclinations)
+
+    # check the shape given the inputs
+    assert flares.shape == (len(t), n_spots,n_inclinations)
+
+    # minimum flux should be larger or equal 1, i.e. the quiescent level
+    assert np.min(flares) >= 1.
+
+    # in n_spots is a weird float override and mention:
+    mywarning = ("Number of spots not an integer. "
+                     "Value will be floored to next integer.")
+    with pytest.warns(UserWarning, match=mywarning):
+        flare_contrast(t, 2.3, emin, emax, alpha, beta, n_inclinations)
+        
+
+    # ONE SPOT
+    # generate lcs from floats with n_spots==1
+    n_spots = 1
+    flares = flare_contrast(t, n_spots, 1, 10, -2, 30, n_inclinations)
+
+    # check the shape given the inputs
+    assert flares.shape == (len(t), n_spots, n_inclinations)
+
+    # minimum flux should be larger or equal 1, i.e. the quiescent level
+    assert np.min(flares) >= 1.
+    
+    
+@pytest.mark.parametrize("n_spots,emin,emax,alpha,beta",
+                         [(1, [1, 1], [1], [-2], [2]),
+                          (1, [1], [1, 1], [-2], [2]),
+                          (1, [1], [1], [-2, 2], [2]),
+                          (1, [1], [1, 1], [-2], [2, 3]),
+                          ])
+
+def test_flare_contrast2(n_spots, emin, emax, alpha, beta):
+    """Test #2. Make sure function crashed if the 
+    dimensions don't match. Test #1 has more unit tests
+    and also integration tests."""
+    
+    # generate time array to be the same for all
+    t = np.arange(0, 2 * np.pi, 2 * np.pi/600)
+    
+    # raise IndexError if dimensions don't match
+    with pytest.raises(ValueError) as e:
+        flare_contrast(t, n_spots, emin, emax, alpha, beta, 5)
+        assert e == (f"Check inputs: one or multiple of emin, "
+                     f"emax, alpha, beta don't match in size, "
+                     f"which should be {int(np.floor(n_spots))}.")
+
+
+def test_create_flare_light_curve():
+    """This basically call two functions that are
+    individually tested elsewhere: wrapped_aflare and 
+    generate_random_power_law_distribution.
+    """
+    # set up time array
+    time = np.linspace(0,1,1000)
+
+    # create a flare lc
+    lc = create_flare_light_curve(time, 0.1, 1000, -2, 2)
+    
+    # just make sure the shape of the light curve is preserved
+    assert lc.shape == time.shape
+
+    
+
+def test_mock_decompose_ed():
+    """Test both a float and an array input."""
+    # pass an array
+    inputvals = np.array([3., 300., 3000.])
+    a, fwhm = mock_decompose_ed(inputvals)
+
+    # shape preserved
+    assert a.shape[0] == 3
+    assert fwhm.shape[0] == 3
+
+    # dont't mess with the default values!
+    assert (fwhm == 0.01).all()
+    assert (a == inputvals/100.).all()
+
+    # pass a float
+    a, fwhm = mock_decompose_ed(3.)
+
+    # shape must be that of a 1D array
+    assert a.shape[0] == 1
+    assert fwhm.shape[0] == 1
+
+    # dont't mess with the default values!
+    assert fwhm[0] == 0.01
+    assert a[0] == 0.03
 
 def test_wrapped_aflare():
     """Integration tests only.
