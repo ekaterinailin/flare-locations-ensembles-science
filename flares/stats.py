ac548796a73014245d490007d89c0b5ba971ae68
Ekaterina Ilin <eilin@aip.de>
2022-12-16 09:50:56 +0100

    fixed test_stats

--- flares/stats.py
+++ flares/stats.py
@@ -85,6 +85,7 @@ def calibratable_diff_stats(df, group, col, steps, size=200):
     # calculate waiting times
     se = group.apply(lambda x: x[col].diff(periods=steps)).reset_index()
         
+   
     # delete superfluos index
     del se["level_2"]
     se["ed_rec"] = df["ed_rec"]
@@ -92,9 +93,7 @@ def calibratable_diff_stats(df, group, col, steps, size=200):
 
     av_rec_num_flares = int(np.rint(se.groupby("midlat_deg").ed_rec.count().mean()))
     
-#     se = se.dropna()
-    print(se)
-    
+    print(se.shape[0], size, av_rec_num_flares)
     # cut dataframe into groups with similar mid latitude but random inclinations
     cut, bins = pd.cut(se["midlat_deg"],
                        np.linspace(se["midlat_deg"].min(),
@@ -105,7 +104,7 @@ def calibratable_diff_stats(df, group, col, steps, size=200):
     agg = se.groupby(cut)
     
     k = agg.apply(lambda x: x[col].median())
-    print(k)
+    
     s = agg.apply(lambda x: x[col].mean())
 
     sm = agg.apply(lambda x: x[col].std())
--- flares/tests/test_stats.py
+++ flares/tests/test_stats.py
@@ -10,13 +10,13 @@ def test_calibratable_diff_stats():
     """Test two random data sets """
     
     # generate fake large dataset
-    N = 3000 # total number of flares we deal with
-    size = 500 # number of ensembles to generate
+    N = 800 # total number of flares we deal with
+    size = 4 # number of ensembles to generate
     df = pd.DataFrame({"COL":np.random.choice(np.arange(10), size=N),
-                       "col4":np.random.choice(np.arange(10), size=N),
+                       "col4":np.random.choice(np.arange(100), size=N),
                        "col0":np.random.choice(np.arange(10), size=N),
                        "col1":np.random.normal(30, 1, N),
-                       "midlat_deg":np.random.choice(np.linspace(0,90,10), size=N),
+                       "midlat_deg":np.random.choice(np.arange(20)*4, size=N),
                        "ed_rec":np.random.normal(30, 1, N),
                        "tstart":np.random.rand(N) * 100})
 
@@ -26,20 +26,23 @@ def test_calibratable_diff_stats():
     # group by two columns
     group = df.groupby(["col0","midlat_deg"])
 
+    print(group.count())
     # apply the function
     dd, bins = calibratable_diff_stats(df, group, "tstart", 1, size=size)
-
+    print(dd)
     # assert shape comes out  correct
-    assert dd.shape == (5,3)
-    assert bins.shape[0] == N // size
+    assert dd.shape == (4,5)
     
     # all values should be positive if the data are sorted
     assert (dd > 0).all().all()
-    
+
     # check if columns are correct
-    assert (dd.columns.values == ["diff_tstart_kurtosis_stepsize1",
-                                  "diff_tstart_skew_stepsize1",
-                                  "diff_tstart_std_over_mean_stepsize1"]).all()
+    assert (dd.columns.values == ["diff_tstart_median_stepsize1",
+                                  "diff_tstart_mean_stepsize1",
+                                  "diff_tstart_std_stepsize1",
+                                  "diff_tstart_nflares_stepsize1",
+                                  "diff_tstart_nstars_stepsize1",
+                                  ]).all()
 
     # apply the function with stepsize 2
     group = df.groupby(["col0","midlat_deg"])
@@ -47,13 +50,15 @@ def test_calibratable_diff_stats():
 
     # rerun the checks from before 
     # assert shape comes out  correct
-    assert dd.shape == (5,3)
-    assert bins.shape[0] == N // size
+    assert dd.shape == (4,5)
+    assert bins.shape[0] == size + 1
     # all values should be positive if the data are sorted
     assert (dd > 0).all().all()
     # check if columns are correct
-    assert (dd.columns.values == ["diff_tstart_kurtosis_stepsize2",
-                                  "diff_tstart_skew_stepsize2",
-                                  "diff_tstart_std_over_mean_stepsize2"]).all()
-
+    assert (dd.columns.values == ["diff_tstart_median_stepsize2",
+                                  "diff_tstart_mean_stepsize2",
+                                  "diff_tstart_std_stepsize2",
+                                  "diff_tstart_nflares_stepsize2",
+                                  "diff_tstart_nstars_stepsize2",
+                                  ]).all()
 
